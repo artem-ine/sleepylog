31ca10def0b1987970c0663c1a2c2a02f4722b45
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-13 13:49:47 +0200

    database reset

--- backend/bin/start_server.sh
+++ backend/bin/start_server.sh
@@ -9,11 +9,11 @@ bundle install
 # bundle exec rake assets:precompile
 
 # # Run database migrations
-# bundle exec rails db:create
-# bundle exec rails db:migrate
+bundle exec rails db:create
+bundle exec rails db:migrate
 
-# # Seed the database (if needed)
-# bundle exec rails db:seed
+# Seed the database (if needed)
+bundle exec rails db:seed
 
 
 # Start the Rails server
