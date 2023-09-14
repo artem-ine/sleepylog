50498b6db19a0483bfaa913e3c23569dbf47ab03
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-14 09:53:27 +0200

    base url fix

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "/api";
 
 const fetchApi = async (endpoint, options = {}) => {
   const response = await fetch(`${BASE_URL}${endpoint}`, options);
