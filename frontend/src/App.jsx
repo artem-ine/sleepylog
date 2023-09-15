58972ecfc1aabef1ee133bbb76590e165f4a5e7c
katamaric <katherinehcao@gmail.com>
2023-09-15 11:00:30 +0200

    cors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import SignupForm from "./components/SignupForm";
 
 function App() {
   return (
     <main className="bg-white dark:bg-black text-black dark:text-white">
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <SignupForm />
       <Footer />
     </main>
   );
