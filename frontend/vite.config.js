ea562e649948d9ca9c12c173bc46c4e881a3e4a0
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-15 22:17:21 +0200

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
       "/api": {
         target: `http://localhost:${railsPort}`,
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
       },
     },
