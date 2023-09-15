4637d4e5e9ec0ebffe6130ef0f4444be70ae6a93
katamaric <katherinehcao@gmail.com>
2023-09-15 11:00:30 +0200

    signup form glow-up

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -28,13 +28,72 @@ function SignupForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <input type="password_confirmation" placeholder="Password Confirmation" value={password_confirmation} onChange={(e) => setPassword_Confirmation(e.target.value)} />
-      <button type="submit">Register</button>
-    </form>
+    <div className="flex justify-center items-center mt-20">
+      <div className="w-full max-w-xs">
+        <form className="bg-primary shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-black text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-black text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-black text-sm font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-black text-sm font-bold mb-2" htmlFor="passwordConfirmation">
+              Password Confirmation
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+              id="password_Confirmation"
+              type="password"
+              placeholder="Password Confirmation"
+              value={password_confirmation}
+              onChange={(e) => setPassword_Confirmation(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Sign up !
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
