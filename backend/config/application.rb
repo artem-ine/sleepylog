1f26f6b4c1cc2b85f360d9e6d9b2a8c1c1b8c6f0
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-18 18:18:48 +0200

    dotenv rails

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -2,15 +2,23 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
+
+
 
 module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
