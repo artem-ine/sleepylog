a89d4b7407ac60631e0a0afa5502c29ff1be9df1
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-20 10:42:12 +0200

    datepicker added

--- frontend/src/components/Dashboard/Entries Modal/Entries.jsx
+++ frontend/src/components/Dashboard/Entries Modal/Entries.jsx
@@ -3,6 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../utils/useAuth";
 import useErrorHandler from "../../../utils/errorHandler";
 import PropTypes from "prop-types";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./datepicker.css";
 
 function EntryForm({ onEntrySuccess }) {
   const navigate = useNavigate();
@@ -98,13 +101,15 @@ function EntryForm({ onEntrySuccess }) {
             >
               Start Time:
             </label>
-            <input
-              type="datetime-local"
+            <DatePicker
               id="start_time"
               className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-black text-sm leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Start Time"
-              value={start_time}
-              onChange={(e) => setStartTime(e.target.value)}
+              selected={start_time}
+              onChange={(date) => setStartTime(date)}
+              showTimeSelect
+              timeFormat="HH:mm"
+              dateFormat="dd/MM/yyyy HH:mm"
+              placeholderText="Select Start Time"
             />
           </div>
           <div className="mb-4">
@@ -114,13 +119,15 @@ function EntryForm({ onEntrySuccess }) {
             >
               End Time:
             </label>
-            <input
-              type="datetime-local"
-              id="end_time"
+            <DatePicker
+              id="start_time"
               className="bg-white shadow appearance-none border rounded-xl w-full py-2 px-3 text-black text-sm leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="End Time"
-              value={end_time}
-              onChange={(e) => setEndTime(e.target.value)}
+              selected={end_time}
+              onChange={(date) => setEndTime(date)}
+              showTimeSelect
+              timeFormat="HH:mm"
+              dateFormat="dd/MM/yyyy HH:mm"
+              placeholderText="Select Start Time"
             />
           </div>
           <div className="mb-4">
