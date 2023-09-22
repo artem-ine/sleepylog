d1f885b4c047fd0e9c8e8e63645feeef91f5c3e1
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-22 18:21:14 +0200

    leftover slash

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'https://sleepylog-2c21ce22bd2f.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://sleepylog-2c21ce22bd2f.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
