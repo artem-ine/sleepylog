da408e8ef62333fb89c011c5616188f6f1a6da36
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-22 12:10:58 +0200

    tab views

--- frontend/src/components/Dashboard/Stats/Stats.jsx
+++ frontend/src/components/Dashboard/Stats/Stats.jsx
@@ -4,24 +4,49 @@ import SleepDurationPastMonth from "./SleepDurationPastMonth";
 import AverageRatingPastWeek from "./AverageRatingWeek";
 import AverageRatingPastMonth from "./AverageRatingMonthly";
 import { useAuth } from "../../../utils/useAuth";
+import { useState } from "react";
 
 function Stats() {
   const { auth } = useAuth();
-  console.log(auth.user);
+  const [selectedTab, setSelectedTab] = useState("past-week");
 
   return (
-    <div>
-      <SleepDurationPastMonth />
-      <AverageRatingPastMonth />
-      <div className="flex flex-row gap-5">
-        <AverageRatingPastWeek />
-        <SleepDurationPastWeek />
-      </div>
-      <h1 className="text-sm font-bold">
-        Looking for a specific period of time?
+    <>
+      <h1 className="">
+        On the run and looking for quick stats for a specific period of time?
       </h1>
       <CustomRange />
-    </div>
+
+      <button
+        onClick={() => setSelectedTab("past-week")}
+        className={
+          selectedTab === "past-week" ? "active pr-10 font-bold" : "pr-10"
+        }
+      >
+        Stats past week
+      </button>
+      <button
+        onClick={() => setSelectedTab("past-month")}
+        className={
+          selectedTab === "past-month" ? "active pr-10 font-bold" : "pr-10"
+        }
+      >
+        Stats past month
+      </button>
+
+      {selectedTab === "past-week" && (
+        <div className="flex flex-row gap-5">
+          <AverageRatingPastWeek />
+          <SleepDurationPastWeek />
+        </div>
+      )}
+      {selectedTab === "past-month" && (
+        <div className="flex flex-row gap-5">
+          <AverageRatingPastMonth />
+          <SleepDurationPastMonth />
+        </div>
+      )}
+    </>
   );
 }
 
