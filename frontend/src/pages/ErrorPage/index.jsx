cf374ab1c3629df8f7a6cdff56a4d2e50a8fafcc
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-22 18:50:10 +0200

    error handler

--- frontend/src/pages/ErrorPage/index.jsx
+++ frontend/src/pages/ErrorPage/index.jsx
@@ -1,17 +1,26 @@
 import { useRouteError } from "react-router-dom";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Navbar />
+      <main
+        id="error-page"
+        className="bg-white dark:bg-black text-black dark:text-white flex flex-col items-center justify-center"
+      >
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </main>
+      <Footer />
+    </>
   );
 }
 
