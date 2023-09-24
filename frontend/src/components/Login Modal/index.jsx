20dbb4c8adb72ce4e1b1a1d428210d37562e746d
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-24 22:29:51 +0200

    reset password now a modal

--- frontend/src/components/Login Modal/index.jsx
+++ frontend/src/components/Login Modal/index.jsx
@@ -14,26 +14,24 @@ const LogInModal = ({ isOpen, onRequestClose }) => {
       className=".custom-modal"
     >
       {isPasswordResetRequested ? (
-        // If password reset is requested, show the reset request form
         <PasswordResetRequestForm
           onRequestClose={() => setPasswordResetRequested(false)}
         />
       ) : (
-        // Otherwise, show the login form
         <LoginForm onLoginSuccess={onRequestClose} />
       )}
-      <div className="modal-actions">
+      <div className="modal-actions gap-3">
         <button
           onClick={onRequestClose}
           className="bg-secondary border border-black hover:border-accent font-bold text-white text-sm py-2 px-4 rounded-xl"
         >
-          Close
+          x
         </button>
         <button
-          onClick={() => setPasswordResetRequested(true)} // Show the reset form when clicked
+          onClick={() => setPasswordResetRequested(true)}
           className="bg-secondary border border-black hover:border-accent font-bold text-white text-sm py-2 px-4 rounded-xl"
         >
-          Forgotten your password?
+          Forgot your password?
         </button>
       </div>
     </Modal>
