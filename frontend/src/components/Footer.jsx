8d15cd29fc1495804a8530a931b4082bd19cc01f
katamaric <katherinehcao@gmail.com>
2023-09-27 00:13:32 +0200

    about page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Footer() {
   return (
     <footer
       id="footer"
       className="bottom-0 w-full bg-primary dark:bg-secondary text-dark dark:text-white"
     >
-      <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-center">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <div className="w-full mx-auto max-w-screen-xl p-4 flex justify-around">
+        <span className="text-sm sm:text-center">
           <p>
             kat & zeke // doodles by hummy
           </p>
@@ -13,6 +15,10 @@ function Footer() {
           All Rights Reserved. © 2023
           </p>
         </span>
+        <div className="">
+          <Link to="/" className="text-sm text-right px-3">Home</Link>
+          <Link to="/about" className="text-sm text-right">About</Link>
+        </div>
       </div>
     </footer>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import PasswordReset from "./pages/Home/PasswordReset";
 import ErrorPage from "./pages/ErrorPage/index.jsx";
 import "./index.css";
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
   {
     path: "/users/password/edit",
     element: <PasswordReset />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/about",
+    element: <About />,
     errorElement: <ErrorPage />,
   },
 ]);
