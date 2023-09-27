2f4ca59c835b757e23519d630768dcfb0023aa97
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-09-27 23:21:50 +0200

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,11 +6,17 @@ function Footer() {
       id="footer"
       className="bottom-0 w-full bg-primary dark:bg-secondary text-dark dark:text-white"
     >
-      <div className="w-full mx-auto max-w-screen-xl px-3 p-4 flex justify-around">
-        <div className="text-sm text-right">
-          <Link to="/" className="px-3">Home</Link>
-          <Link to="/about" className="px-3">About</Link>
-          <Link to="/terms-and-conditions" className="px-3">Terms and Conditions</Link>
+      <div className="w-full mx-auto max-w-screen-xl px-3 p-4 flex justify-around gap-3">
+        <div className="text-sm text-right flex">
+          <Link to="/" className="px-3">
+            Home
+          </Link>
+          <Link to="/about" className="px-3">
+            About
+          </Link>
+          <Link to="/terms-and-conditions" className="px-3">
+            Terms and Conditions
+          </Link>
         </div>
         <span className="text-sm sm:text-center">
           <p>kat & zeke // doodles by hummy</p>
